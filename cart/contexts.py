077933c12d093dfc439f70d5acc341fd7cad4d93
PratimaGurav <prtmgrv2@gmail.com>
2022-06-18 18:40:23 +0000

    Added quantity boxes to shopping cart page

--- cart/contexts.py
+++ cart/contexts.py
@@ -19,7 +19,7 @@ def cart_contents(request):
             'quantity': quantity,
             'product': product,
         })
-    
+   
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
