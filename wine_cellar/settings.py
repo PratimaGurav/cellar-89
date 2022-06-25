5b0dd95619955331fb5531e072f77461ce5b1520
PratimaGurav <prtmgrv2@gmail.com>
2022-06-25 14:14:38 +0000

    added confirmation emails

--- wine_cellar/settings.py
+++ wine_cellar/settings.py
@@ -219,3 +219,4 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'cellar-89@example.com'
