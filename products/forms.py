dfc6489dc13297b70a88eaa3268d7a827248d6b6
PratimaGurav <prtmgrv2@gmail.com>
2022-06-26 07:15:44 +0000

    Updated product admin - forms

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,10 @@
+from django import forms
 from django.forms import (
     Textarea, Select, ModelForm, CharField
 )
 from django.core.exceptions import ValidationError
-from .models import Review
+from .models import Review, Product, Category
+
 
 class ReviewForm(ModelForm):
     """
@@ -32,3 +34,19 @@ class ReviewForm(ModelForm):
                 }
             ),
         }
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-1'
