781c2f5b07aa2e028ed826e763b4c0fe389fa61c
PratimaGurav <prtmgrv2@gmail.com>
2022-06-29 18:59:40 +0000

    Adding wishlist to profile page

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
         name='order_history'
     ),
     path(
-        'add_to_wishlist/<int:product_id>',
+        'add_to_wishlist/<int:product_id>/',
         views.add_to_wishlist,
         name='add_to_wishlist'
     ),
     path(
-        'remove_from_wishlist/<int:product_id>',
+        'remove_from_wishlist/<int:product_id>/',
         views.remove_from_wishlist,
         name='remove_from_wishlist'
     ),
