eda9c2070b110d88db5d66fbf49b8b81e435b170
PratimaGurav <prtmgrv2@gmail.com>
2022-07-04 16:31:15 +0000

    Create flyer, modal for wine suggestions

---
+++ products/static/products/js/flyer.js
@@ -0,0 +1,9 @@
+// Function to display and hide message flyer.
+function myFunction() {
+    var x = document.getElementById('flyer');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
