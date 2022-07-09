f6bc5866267416ee8af4afc015913b030bd921f0
PratimaGurav <prtmgrv2@gmail.com>
2022-07-09 06:28:58 +0000

    Fix pep8 line errors for views-cart

--- cart/views.py
+++ cart/views.py
@@ -6,10 +6,12 @@ from products.models import Product
 
 # Create your views here.
 
+
 def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
@@ -18,10 +20,12 @@ def add_to_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
-   
+
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+        messages.success(
+            request, f'Updated {product.name} quantity to {cart[item_id]}'
+            )
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {product.name} to your cart')
@@ -31,15 +35,17 @@ def add_to_cart(request, item_id):
 
 
 def adjust_cart(request, item_id):
-    """ Adjust the quantity of the specified product to the specified amount."""
+    """ Adjust the quantity of the product to the specified amount."""
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
-    
+
     if quantity > 0:
         cart[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+        messages.success(
+            request, f'Updated {product.name} quantity to {cart[item_id]}'
+            )
     else:
         cart.pop(item_id)
         messages.success(request, f'Removed {product.name} from your cart')
