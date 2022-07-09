b9104b736e9d28261d9498f3cf847d610aab16ab
PratimaGurav <prtmgrv2@gmail.com>
2022-07-09 09:03:58 +0000

    Fixed pep8 line errors and added docstrings apps.py profile

--- profiles/apps.py
+++ profiles/apps.py
@@ -1,6 +1,12 @@
+"""
+Config file for profiles app
+"""
 from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
+    """
+    Profiles app Config
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
