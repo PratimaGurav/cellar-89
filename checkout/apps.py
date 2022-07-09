59e0ca22deb9c4094c359b9f539d8585b904d3f0
PratimaGurav <prtmgrv2@gmail.com>
2022-07-09 06:58:46 +0000

    Fixed pep8 line errors and added docstrings apps.py-checkout

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,7 +1,13 @@
+"""
+Config file for checkout app
+"""
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    """
+    Checkout app Config
+    """
     name = 'checkout'
 
     def ready(self):
