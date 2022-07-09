e63578921cbbd27e022dd76457be8e21569df4dd
PratimaGurav <prtmgrv2@gmail.com>
2022-07-09 09:22:42 +0000

    Fixed pep8 line errors and added docstrings custom_storages.py

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,21 @@
+"""
+Defines the storage location for static and
+media files for production
+"""
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+# pylint: disable=W0223
 
 
 class StaticStorage(S3Boto3Storage):
+    """
+    Sets the storage location for static files on AWS
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Sets the storage location for media files on AWS
+    """
     location = settings.MEDIAFILES_LOCATION
